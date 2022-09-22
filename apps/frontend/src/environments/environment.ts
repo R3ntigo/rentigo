26edd9afa385ab19c81c9ecc5193ac326a4767de
sakibesrat71 <esratebtida@iut-dhaka.edu>
2022-09-22 14:40:09 +0600

    linterc issues

--- apps/frontend/src/environments/environment.ts
+++ apps/frontend/src/environments/environment.ts
@@ -1,6 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // When building for production, this file is replaced with `environment.prod.ts`.
 
+// eslint-disable-next-line import/prefer-default-export
 export const environment = {
   production: false,
 };
