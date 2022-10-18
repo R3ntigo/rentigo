0d66dc04b012409468693035c550a41f71ed1af2
Noman5237 <anonyman637@gmail.com>
2022-10-18 23:00:49 +0600

    PN-44 removed unnecessary axios call

--- apps/frontend/src/app/home/home.tsx
+++ apps/frontend/src/app/home/home.tsx
@@ -1,13 +1,3 @@
-import axios from 'axios';
-import { useEffect } from 'react';
-
-const Home = () => {
-	useEffect(() => {
-		axios.get('/api').then((res) => {
-			console.log(res.data);
-		});
-	}, []);
-	return <p>Welcome Frontend!</p>;
-};
+const Home = () => <p>Welcome Frontend!</p>;
 
 export { Home };
