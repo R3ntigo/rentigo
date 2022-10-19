70f8d90661ccbd4a579b62c17e1bfaae94f720b1
sakibesrat71 <esratebtida@iut-dhaka.edu>
2022-10-19 11:12:18 +0600

    PN-38 product rent page done without css

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import { ProductUpload } from './product-upload/ProductUpload';
 import { ProductShow } from './product-show/ProductShow';
+import { ProductRentReq } from './product-rent-req/ProductRentReq';
 import { Home } from './home/home';
 import { SignIn } from './signin/sign-in';
 
@@ -11,6 +12,7 @@ const App = () => (
 		<Route path="/signin" element={<SignIn />} />
 		<Route path="/show" element={<ProductShow />} />
 		<Route path="/upload" element={<ProductUpload />} />
+		<Route path="/rent/:id" element={<ProductRentReq />} />
 	</Routes>
 );
 
