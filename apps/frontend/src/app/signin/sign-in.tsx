2350bbb4131c9425301a2514d4f46d116a1004bc
sakibesrat71 <esratebtida@iut-dhaka.edu>
2022-10-21 23:37:03 +0600

    PN-41 did some changes

--- apps/frontend/src/app/signin/sign-in.tsx
+++ apps/frontend/src/app/signin/sign-in.tsx
@@ -4,6 +4,7 @@ import axios, { AxiosError } from 'axios';
 import { SignInDto } from '@rentigo/types/dto';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import TextField from '@mui/material/TextField';
 
 const SignIn = () => {
 	const navigate = useNavigate();
@@ -29,25 +30,62 @@ const SignIn = () => {
 	};
 
 	return (
-		<>
-			<h1>SignIn</h1>
-			<form onSubmit={handleSubmit(onSubmit)}>
-				<label htmlFor="username">
-					Username
-					<input type="text" {...register('username')} />
-				</label>
-				{errors.username && <p>{errors.username.message}</p>}
-				<br />
-				<label htmlFor="password">
-					Password
-					<input type="password" {...register('password')} />
-				</label>
-				{errors.password && <p>{errors.password.message}</p>}
-				<br />
-				<button type="submit">Sign In</button>
-			</form>
-			{errorResponseMessage && <p>{errorResponseMessage}</p>}
-		</>
+		<div className="antialiased text slate-500 dark:text-slate-400 bg-white">
+			<div className="h-screen relative flex items-center justify-center px-16">
+				<div className="relative w-full max-w-lg">
+					<div className="">
+						<div className="absoulute top-0 -left-4 w-72 h-72 bg-purple-300 rounded-full" ></div>
+						<div className="absoulute top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full"></div>
+					</div>
+					<form onSubmit={handleSubmit(onSubmit)}>
+						<label htmlFor="username" className="text-2xl">
+							Username
+							<input
+								type="text"
+								{...register('username')}
+								className="shadow appearance-none
+									border rounded w-full py-2 px-3 text-[#db2777]
+										leading-tight focus:outline-[#10b981]"
+							/>
+						</label>
+						{errors.username
+						&& (
+							<p className="mt-2 text-sm text-red-600 dark:text-red-500 animate-pulse">
+								{errors.username.message}
+							</p>
+						)}
+						<br />
+						<label htmlFor="password" className="text-2xl">
+							Password
+							<input
+								type="password"
+								{...register('password')}
+								className="shadow appearance-none
+									border rounded w-full py-2 px-3 text-[#db2777]
+										leading-tight focus:outline-[#10b981]"
+							/>
+						</label>
+						<br />
+						{errors.password
+						&& (
+							<p className="mt-2 text-sm text-red-600 dark:text-red-500 animate-pulse">
+								{errors.password.message}
+							</p>
+						)}
+						<br />
+						<button
+							type="submit"
+							className="bg-transparent hover:bg-[#f59e0b] text-[#f59e0b] font-semibold
+							hover:text-white py-2 px-4 border border-[#f59e0b] hover:border-transparent rounded"
+						>
+							Sign In
+						</button>
+					</form>
+				</div>
+				{errorResponseMessage && <p>{errorResponseMessage}</p>}
+			</div>
+		</div>
+
 	);
 };
 
