d2c531aedbe848fc64be69238fd23d6332fc6037
sakibesrat71 <esratebtida@iut-dhaka.edu>
2022-11-05 22:20:54 +0600

    PN-45 dashboard design completed

--- apps/frontend/src/app/dashboard/Dashboard.tsx
+++ apps/frontend/src/app/dashboard/Dashboard.tsx
@@ -1,5 +1,16 @@
+import { TfiLayoutMediaRight } from 'react-icons/tfi';
+import { BsChatLeftText } from 'react-icons/bs';
+import { RiLoginBoxLine } from 'react-icons/ri';
+import { MdOutlineProductionQuantityLimits } from 'react-icons/md';
+import { GrNotification } from 'react-icons/gr';
+import { IoMdStats } from 'react-icons/io';
+import { MdOutlinePayments } from 'react-icons/md';
+
 const Dashboard = () => {
 	const ll = 0;
+	const sayHello = () => {
+		console.log('Hello');
+	};
 	return (
 		<>
 			<div className="absolute top-6 w-full text-center">
@@ -19,17 +30,64 @@ const Dashboard = () => {
 			bg-pink-300 mix-blend-multiply filter blur-xl opacity-70 "
 				/>
 			</div>
-			<div className="border-spacing-4 h-10"></div>
-			<div className="grid grid-cols-2 gap-4 px-10 h-screen">
-				<div className="bg-slate-100 rounded-lg border-4 border-solid border-yellow-300">01</div>
-				<div className="bg-slate-100 rounded-lg border-4 border-solid border-purple-400">02</div>
-				<div className="bg-slate-100 rounded-lg border-4 border-solid border-pink-600">03</div>
-				<div className="col-span-2 bg-slate-100 rounded-lg border-4 border-solid border-green-500">04</div>
-				<div className="bg-slate-100 rounded-lg border-4 border-solid border-blue-700">05</div>
-				<div className="bg-slate-100 rounded-lg border-4 border-solid border-[#ff7c01]">06</div>
-				<div className="col-span-2 bg-slate-100 rounded-lg border-4 border-solid border-[#00dcf0]">07</div>
-				<div />
-				<div />
+			<div className="border-spacing-4 h-10" />
+			<div className="grid grid-cols-2 gap-4 px-10 h-fit">
+				<div className="bg-slate-100 rounded-lg p-1 border-4 border-solid border-yellow-300">
+					<div className="flex justify-end">
+						<div className="px-2">
+							<TfiLayoutMediaRight size="64" color="yellow[300]" />
+						</div>
+					</div>
+					<div className=" top-20 left-3 text-left font-semibold text-xl">Request for rent</div>
+				</div>
+				<div className="bg-slate-100 rounded-lg p-1 border-4 border-solid border-purple-400" onClick={sayHello}>
+					<div className="flex justify-end">
+						<div className="p-3">
+							<BsChatLeftText size="44" color="yellow[300]" />
+						</div>
+					</div>
+					<div className=" relativeoff top-5 left-3 text-left font-semibold text-xl">Inbox</div>
+				</div>
+				<div className="bg-slate-100 rounded-lg p-1 border-4 border-solid border-pink-600">
+					<div className="flex justify-end">
+						<div className="px-2">
+							<RiLoginBoxLine size="44" color="yellow[300]" />
+						</div>
+					</div>
+					<div className="relativeoff px-1 left-3 text-left font-semibold text-xl">My Requested Products</div>
+				</div>
+				<div className="col-span-2 bg-slate-100 p-1 rounded-lg border-4 border-solid border-green-500">
+					<div className="flex justify-end">
+						<div className="px-2">
+							<MdOutlineProductionQuantityLimits size="44" color="yellow[300]" />
+						</div>
+					</div>
+					<div className="text-left font-semibold text-xl">Listed Products  </div>
+				</div>
+				<div className="bg-slate-100 p-1 rounded-lg border-4 border-solid border-blue-700">
+					<div className="flex justify-end">
+						<div className="px-2">
+							<GrNotification size="38" color="yellow[300]" />
+						</div>
+					</div>
+					<div className="  p-2 text-left font-semibold text-xl">Notifications </div>
+				</div>
+				<div className="bg-slate-100 rounded-lg border-4 border-solid border-[#ff7c01]">
+					<div className="flex justify-end">
+						<div className="px-2">
+							<IoMdStats size="44" color="yellow[300]" />
+						</div>
+					</div>
+					<div className="  p-2 text-left font-semibold text-xl">Stats </div>
+				</div>
+				<div className="col-span-2 p-1 bg-slate-100 rounded-lg border-4 border-solid border-[#00dcf0]">
+					<div className="flex justify-end">
+						<div className="px-2">
+							<MdOutlinePayments size="44" color="yellow[300]" />
+						</div>
+					</div>
+					<div className=" font-semibold text-xl">Payments </div>
+				</div>
 
 			</div>
 		</>
