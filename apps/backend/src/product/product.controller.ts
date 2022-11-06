6c447348440b657769e70aabb1c363a4e5a83d45
SazidAF <sazidfarhan.a7c@gmail.com>
2022-11-06 23:41:34 +0600

    PN-57 delete and update methods in controller

--- apps/backend/src/product/product.controller.ts
+++ apps/backend/src/product/product.controller.ts
@@ -1,16 +1,27 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Req, Res, Param, Delete, Put} from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
+import { Request, Response} from 'express';
 
 @Controller('product')
 export class ProductController {
     @Get()
-    findOne(): string {
-        return 'Get One Items'; 
+    findOne(@Param('id') id): string {
+        return `Product ID: ${id}`; 
     }
 
     @Post()
     createProduct(@Body() createProductDto: CreateProductDto): string {
         return `Name: ${createProductDto.name}`;
+    }
+
+    @Delete()
+    deleteOne(@Param('id') id): string {
+        return `Delete ${id}`;
+    }
+
+    @Put()
+    updateOne(@Body() updateProductDto: CreateProductDto, @Param('id') id): string {
+        return `Update ${id} - Name: ${updateProductDto}`;
     }
 
 
