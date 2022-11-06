b4e504304d1d3edd2759649f7803e6d48ef62397
SazidAF <sazidfarhan.a7c@gmail.com>
2022-11-06 23:27:07 +0600

    PN-57 CreateProductDto template, findOne, CreateProduct

--- apps/backend/src/product/product.controller.ts
+++ apps/backend/src/product/product.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+    @Get()
+    findOne(): string {
+        return 'Get One Items'; 
+    }
+
+    @Post()
+    createProduct(@Body() createProductDto: CreateProductDto): string {
+        return `Name: ${createProductDto.name}`;
+    }
+
+
+}
