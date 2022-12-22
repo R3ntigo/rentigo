87e2f6df6a1beb609b0957869928d37f0d16ef6a
sakibesrat71 <esratebtida@iut-dhaka.edu>
2022-12-23 00:14:55 +0600

    PN-45 listed products pag completed in dashboard

--- apps/frontend/src/app/dashboard/ListedProduct.tsx
+++ apps/frontend/src/app/dashboard/ListedProduct.tsx
@@ -9,13 +9,14 @@ import {
 	Tag,
 	Resource
 } from '@rentigo/models';
-import { Gender } from '@rentigo/dto';
+import { Gender } from '@rentigo/types';
+import { ProductTile } from './ProductTile';
 
 const ListedProduct = () => {
 	const totalNumberofProducts = 10; // this should be fetched from the backend
 	const curruntlyListedProducts = 7; // this should be fetched from the backend
-	const availableProducts = 3;
-	const demoProduct: Product = {
+	const availableProducts = totalNumberofProducts - curruntlyListedProducts;
+	const demoProduct: Product[] = [{
 		id: '1',
 		description: 'demo description',
 		title: 'demo',
@@ -70,7 +71,7 @@ const ListedProduct = () => {
 		}],
 		totalQuantity: 10,
 		availableQuantity: 10
-	};
+	}];
 	const getProducts = async () => {
 		const res = await fetch('http://localhost:3333/api/product');
 		const data = await res.json();
@@ -100,7 +101,7 @@ const ListedProduct = () => {
 			bg-accent1 mix-blend-multiply filter blur-xl opacity-70 "
 				/>
 			</div>
-			<div className="p-10">
+			<div className="p-10 ">
 				<div className="border-dashed border-2 border-[#f59e0b] rounded-2xl p-2 grid grid-cols-3 gap-2 ">
 					<div>
 						<p className="text-center">Total Products</p>
@@ -115,8 +116,15 @@ const ListedProduct = () => {
 						<p className="text-center text-2xl font-semibold">{availableProducts}</p>
 					</div>
 				</div>
-
+				{
+					demoProduct.map((product) => (
+						<div key={product.id} className="py-4">
+							<ProductTile product={product} />
+						</div>
+					))
+				}
 			</div>
+
 		</>
 	);
 };
