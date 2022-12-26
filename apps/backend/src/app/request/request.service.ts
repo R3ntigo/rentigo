6d4d439f34fee73170f27d005159a4297745afea
SazidAF <sazidfarhan.a7c@gmail.com>
2022-12-26 23:40:46 +0600

    PN-66 createRequestDto

--- apps/backend/src/app/request/request.service.ts
+++ apps/backend/src/app/request/request.service.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
+import { FindOptionsRelations, In } from 'typeorm';
+
+import { Request, User } from '@rentigo/models';
+import { CreateRequestDto, UpdateRequestDto } from '@rentigo/dto';
+
+import { Operations } from '@rentigo/constants';
+
+import { RentingPolicyRepository } from './renting-policy.repository';
+
 
 @Injectable()
 export class RequestService {}
