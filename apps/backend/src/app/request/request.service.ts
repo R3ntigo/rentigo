f67047498ab3085b54d3ebf3018044627187295f
SazidAF <sazidfarhan.a7c@gmail.com>
2022-12-27 00:59:46 +0600

    PN-66 request services

--- apps/backend/src/app/request/request.service.ts
+++ apps/backend/src/app/request/request.service.ts
@@ -19,12 +19,69 @@ export class RequestService {
 	) {}
 
 	async create(user: User, createRequestDto: CreateRequestDto): Promise<Request> {
-		const request = await this.dtoToEntity(createRequestDto);
+		const request = await this.CreateDtoToEntity(createRequestDto);
 		request.borrower = user;
 		return this.requestRepository.save(request);
 	}
 
-	private async dtoToEntity(dto: CreateRequestDto): Promise<Request> {
+	async findAll(): Promise<Request[]> {
+		return this.requestRepository.find();
+	}
+
+	async findOne(id: string, relations: FindOptionsRelations<Request> = {}): Promise<Request> {
+		const request = this.requestRepository.findOne({ where: { id }, relations });
+		if (!request) {
+			throw new NotFoundException();
+		}
+		return request;
+	}
+
+	async update(user: User, updateRequestDto: UpdateRequestDto): Promise<Request> {
+		let request = await this.findOne(updateRequestDto.id);
+		if (request.borrower.id !== user.id) {
+			throw new ForbiddenException();
+		}
+		const updatedRequest = await this.partialDtoToEntity(updateRequestDto);
+		updatedRequest.id = request.id;
+		request = await this.requestRepository.save(updatedRequest);
+		return request;
+	}
+
+	async remove(user: User, id: string): Promise<Request> {
+		const isPermitted = await this.hasPermissionTo(Operations.DELETE, user, id);
+		if (!isPermitted) {
+			throw new ForbiddenException();
+		}
+		return this.requestRepository.softRemoveOneBy({ id });
+	}
+
+	async hasPermissionTo(_: Operations, user: User, id: string) {
+		try {
+			const userWithProduct = await this.requestRepository.findOne({
+				relations: { borrower: true },
+				where: { id, borrower: { id: user.id } },
+			});
+			return !!userWithProduct;
+		} catch (error) {
+			return false;
+		}
+	}
+
+	private async CreateDtoToEntity(dto: CreateRequestDto): Promise<Request> {
+		const product = await this.productService.findOne(dto.product);
+		const address = await this.addressService.findOne(dto.address);
+		const request: Request = {
+			product,
+			quantity: dto.quantity,
+			address,
+			duration: dto.duration,
+			borrower: null,
+			status: RequestStatus.PENDING,
+		};
+		return request;
+	}
+
+	private async partialDtoToEntity(dto: UpdateRequestDto): Promise<Request> {
 		const product = await this.productService.findOne(dto.product);
 		const address = await this.addressService.findOne(dto.address);
 		const request: Request = {
