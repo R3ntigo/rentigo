411a49cce996cd56c37053a7de4e3723d19c90c0
SazidAF <sazidfarhan.a7c@gmail.com>
2022-12-26 23:13:59 +0600

    PN-66 controller basic setup

--- apps/backend/src/app/request/request.controller.ts
+++ apps/backend/src/app/request/request.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
+import { ReqUser } from '@rentigo/decorators';
+import { CreateRequestDto, UpdateRequestDto } from '@rentigo/dto';
+import { Request, User } from '@rentigo/models';
+
+import { JwtAuthGuard } from '../auth';
+
+import { RequestService } from './request.service';
+
+@ApiTags('Request')
 @Controller('request')
-export class RequestController {}
+@UseGuards(JwtAuthGuard)
+export class RequestController {
+	constructor(private readonly requestService: RequestService){}
+
+	@Post()
+	Create(@Body() createRequestDto: CreateRequestDto, @ReqUser user: User): Promise<Request> {
+		return this.requestService.create(user, createRequestDto);
+	}
+
+	@Get()
+	findAll(): Promise<Request[]> {
+		return this.requestService.findAll();
+	}
+
+	@Get(':id')
+	findOne(@Param('id') id: string): Promise<RentingPolicy> {
+		return this.requestService.findOne(id);
+	}
+
+	@Patch()
+	update(@Body() updateRentingPolicyDto: UpdateRentingPolicyDto, @ReqUser() user: User): Promise<RentingPolicy> {
+		return this.requestService.update(user, updateRentingPolicyDto);
+	}
+
+	@Delete(':id')
+	remove(@Param('id') id: string, @ReqUser() user: User): Promise<RentingPolicy> {
+		return this.requestService.remove(user, id);
+	}
+}
