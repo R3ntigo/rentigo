01d471b493f37e8e2cb87ab2d0ecb7c03e28f0f1
Noman5237 <anonyman637@gmail.com>
2022-12-30 15:31:56 +0600

    PN-64 add auth

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -2,12 +2,11 @@ import { Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import { ProductRentReqWithAuth } from './product-rent-req/ProductRentReq';
-import { Home } from './home/home';
 import { ProductUploadWithAuth, ProductUpload } from './product-upload/ProductUpload';
 import { ProductShowWithAuth } from './product-show/ProductShow';
 import { enableFreezeUI } from './util/freezeui/freezeui';
 import { SideBar } from './navbar/Navbar';
-import { Dashboard } from './dashboard/Dashboard';
+import { DashboardWithAuth } from './dashboard/Dashboard';
 import { SignIn2 } from './signin/sign-in-new';
 import { ListedProduct } from './dashboard/ListedProduct';
 import { RequestforRentWithAuth } from './dashboard/RequestforRent';
@@ -20,6 +19,7 @@ import { ConfirmEmail } from './register/confirm-email';
 import { VerifyEmail } from './register/verify-emai';
 import { ConfirmPassword } from './register/verify-password';
 import { AddNewAddress } from './product-upload/addressUpload';
+import { SignOutWithAuth } from './signin/sign-out';
 
 enableFreezeUI();
 
@@ -29,14 +29,15 @@ const App = () => (
 			<SideBar />
 			<div className="grow h-full">
 				<Routes>
-					<Route path="/" element={<Dashboard />} />
+					<Route path="/" element={<DashboardWithAuth />} />
 					<Route path="/sign-in" element={<SignIn2 />} />
+					<Route path="/sign-out" element={<SignOutWithAuth />} />
 					<Route path="/show/:id" element={<ProductShowWithAuth />} />
 					<Route path="/upload" element={<ProductUploadWithAuth />} />
 					<Route path="/uploadinsecured" element={<ProductUpload />} />
 					<Route path="/rent/:id" element={<ProductRentReqWithAuth />} />
 					<Route path="/testNavbar" element={<SideBar />} />
-					<Route path="/dashboard" element={<Dashboard />} />
+					<Route path="/dashboard" element={<DashboardWithAuth	/>} />
 					<Route path="/listed-product" element={<ListedProduct />} />
 					<Route path="/rent-req-dash" element={<RequestforRentWithAuth />} />
 					<Route path="/rent-req-detail/:id" element={<RequestDetailWithAuth />} />
@@ -46,7 +47,7 @@ const App = () => (
 					<Route path="/register/confirm-email" element={<ConfirmEmail />} />
 					<Route path="/register/verify-email/:id" element={<VerifyEmail />} />
 					<Route path="/register/verify-password" element={<ConfirmPassword />} />
-					<Route path="/register/complete" element={<Dashboard />} />
+					<Route path="/register/complete" element={<DashboardWithAuth />} />
 					<Route path="/add-new-address" element={<AddNewAddress />} />
 					<Route path="*" element={<div>404</div>} />
 				</Routes>
